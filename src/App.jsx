d18600cc5cde2545fb77056074a8046fef90f551
AnishKumar100 <akcoder1102004@gmail.com>
2025-06-13 00:52:50 +0530

    added vercel's performance view command in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Header,Footer} from "../src/components/indexComponents"
 import { AllRoutes } from './routes/AllRoutes'
+import { Analytics } from "@vercel/analytics/next"
 function App() 
 {
  return(<>
